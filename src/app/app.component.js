47dd639ced2e774d92ee6e2fb708e1a45b6a7f25
Dennis Adrians <DA@office.fortech.de>
2017-05-03 13:18:07 +0200

    Hello Angular 4

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -14,7 +14,7 @@ var AppComponent = (function () {
 }());
 AppComponent = __decorate([
     core_1.Component({
-        selector: 'my-app',
+        selector: 'single-page-app',
         template: "<h1>Hello {{name}}</h1>"
     })
 ], AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component
 ({
-  selector: 'my-app',
+  selector: 'single-page-app',
   template: `<h1>Hello {{name}}</h1>`
 })
 export class AppComponent  
